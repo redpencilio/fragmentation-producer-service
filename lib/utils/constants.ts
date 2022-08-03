1c53935af988bd2de2bd68076bc0d1dd6518c6b5
Elena Poelman <elena.poelman@redpencil.io>
2022-08-03 12:09:18 +0200

    update README

--- lib/utils/constants.ts
+++ lib/utils/constants.ts
@@ -8,15 +8,14 @@ export const PAGE_RESOURCES_COUNT = parseInt(
   process.env.PAGE_RESOURCES_COUNT || '10'
 );
 export const STREAM_PREFIX = namespace(
-  process.env.LDES_STREAM_PREFIX || 'http://example.com/'
+  process.env.LDES_STREAM_PREFIX || 'http://mu.semte.ch/streams/'
 );
 export const TIME_TREE_RELATION_PATH =
   process.env.TIME_TREE_RELATION_PATH ||
   'http://www.w3.org/ns/prov#generatedAtTime';
 
 export const PREFIX_TREE_RELATION_PATH =
-  process.env.PREFIX_TREE_RELATION_PATH ||
-  'https://w3id.org/tree#PrefixRelation';
+  process.env.PREFIX_TREE_RELATION_PATH || 'https://example.org/name';
 
 export const CACHE_SIZE = parseInt(process.env.CACHE_SIZE || '10');
 
