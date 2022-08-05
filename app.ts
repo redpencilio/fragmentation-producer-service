91db146baae7bddbdc7c80620be49d38661cec22
Elena Poelman <elena.poelman@redpencil.io>
2022-08-05 11:52:37 +0200

    add ability to enable basic auth with env vars

--- app.ts
+++ app.ts
@@ -2,6 +2,12 @@ const { app, errorHandler } = require('mu-javascript-library');
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { addMember, getNode } from './lib/controller';
+import type { Request, Response, NextFunction } from 'express';
+import {
+  AUTH_PASSWORD,
+  AUTH_USERNAME,
+  ENABLE_AUTH,
+} from './lib/utils/constants';
 
 app.use(cors());
 app.use(
@@ -12,7 +18,23 @@ app.use(
   })
 );
 
-app.post('/:folder', addMember);
+const validateUser = (req: Request, res: Response, next: NextFunction) => {
+  if (ENABLE_AUTH) {
+    if (req.headers.authorization?.startsWith('Basic ')) {
+      const b64value = req.headers.authorization.split(' ')[1];
+      const [username, password] = Buffer.from(b64value, 'base64')
+        .toString()
+        .split(':');
+      if (username === AUTH_USERNAME && password === AUTH_PASSWORD) {
+        return next();
+      }
+    }
+    return res.status(401).send('Authentication failed.');
+  }
+  return next();
+};
+
+app.post('/:folder', validateUser, addMember);
 
 app.get('/:folder*/:nodeId', getNode);
 
